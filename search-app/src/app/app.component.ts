707415a3a0f04d9f7f387684ac3358784517307d
sanjaykig <40465737+sanjaykig@users.noreply.github.com>
2019-11-24 10:43:31 -0500

    Post Request Fucntionality added
    Add a listener on backed server and change the "post_url" in app.component.ts suitably to receive json object.

--- search-app/src/app/app.component.ts
+++ search-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'search-app';
+
+  constructor(private http: HttpClient) { }
+
+  tcode : string;
+  submit() {
+    debugger;
+    return this.http.post('http://httpbin.org/post',
+    JSON.stringify({qry: this.tcode})).subscribe(data => {
+
+      console.log("POST Request is successful ", data);
+      
+      },
+    error => {
+      console.log(JSON.stringify(error.json()));
+    }
+    )
+
+  }
 }
+
